991e1cbf70ca974651f566d4fff2fe81ab7c4fc3
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2022-11-20 18:58:48 -0800

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,38 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://arcade.makecode.com/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+    /**
+     * TODO: describe your function here
+     * @param n is a number that determines a # of dings, eg: 5
+     */
+    //% block
+    export function manyDings(num: number) {
+        for (let index = 0; index < num; index++) {
+            music.baDing.play()
+        }
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
---
+++ main.ts
@@ -0,0 +1,3 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    custom.manyDings(0)
+})
